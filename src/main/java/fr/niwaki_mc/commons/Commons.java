b0cb6dad8abe5df68bb6ae51b785eee8eff96388
Alexis Beyrand <wither.plugins@gmail.com>
2024-05-04 23:36:04 +0200

    Initialisation du mod

---
+++ src/main/java/fr/niwaki_mc/commons/Commons.java
@@ -0,0 +1,6 @@
+package fr.niwaki_mc.commons;
+
+public class Commons {
+
+    public static final String MOD_ID = "niwaki";
+}
---
+++ src/main/java/fr/niwaki_mc/mod/NiwakiMod.java
@@ -0,0 +1,59 @@
+package fr.niwaki_mc.mod;
+
+import com.mojang.logging.LogUtils;
+import fr.niwaki_mc.commons.Commons;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(Commons.MOD_ID)
+public class NiwakiMod
+{
+
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public NiwakiMod()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+
+        modEventBus.addListener(this::addCreative);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event)
+    {
+
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event)
+    {
+
+    }
+
+    @Mod.EventBusSubscriber(modid = Commons.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+
+        }
+    }
+}
